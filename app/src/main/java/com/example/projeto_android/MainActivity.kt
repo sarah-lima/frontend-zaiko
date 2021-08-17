08a37c4eeacc61712f7c13920f67416cb61c3cad
sarah-lima <sarah.lima@bandtec.com.br>
2021-08-17 09:30:25 -0300

    telas de primeiro acesso

--- app/src/main/java/com/example/projeto_android/MainActivity.kt
+++ app/src/main/java/com/example/projeto_android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.projeto_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import kotlinx.android.synthetic.main.one_view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +11,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Handler().postDelayed(Runnable
         {
-            setContentView(R.layout.initial)
+            setContentView(R.layout.one_view)
         }, 3000
         )
+        setContentView(R.layout.one_view)
+        val primeiroAcesso = button2
+        primeiroAcesso.setOnClickListener {
+            setContentView(R.layout.one_view_2)
+        }
+        
     }
 }
