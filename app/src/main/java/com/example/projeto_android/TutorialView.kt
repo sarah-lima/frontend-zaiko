d5308ed29c1d99f4d12ee6c2cca6bd624a412cf2
sarah-lima <sarah.lima@bandtec.com.br>
2021-08-18 21:14:47 -0300

    novas telas adicionadas

--- app/src/main/java/com/example/projeto_android/TutorialView.kt
+++ app/src/main/java/com/example/projeto_android/TutorialView.kt
@@ -1,36 +1,24 @@
 package com.example.projeto_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.buttonlayout.*
 import kotlinx.android.synthetic.main.one_view.*
 
-class MainActivity : AppCompatActivity() {
+class TutorialView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Handler().postDelayed(Runnable
-        {
-            setContentView(R.layout.one_view)
-            continuacao()
-        }, 3000
-        )
+        setContentView(R.layout.one_view)
 
-        
-    }
-    fun continuacao(){
         val segundoTut = button2
         val tercTut = button3
         val quartTut = button4
         val quintTut = button5
         segundoTut.setOnClickListener {
             setContentView(R.layout.one_view_2)
-            continuacao()
         }
         tercTut.setOnClickListener {
             setContentView(R.layout.one_view)
-            continuacao()
         }
     }
 }
