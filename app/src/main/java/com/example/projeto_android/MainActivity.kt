076ce0492b633b5852348ee939f27676c7cb7079
sarah-lima <sarah.lima@bandtec.com.br>
2021-09-22 21:44:37 -0300

    splash screen em andamento

--- app/src/main/java/com/example/projeto_android/MainActivity.kt
+++ app/src/main/java/com/example/projeto_android/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.projeto_android
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,8 +12,79 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Handler().postDelayed(Runnable
         {
-            setContentView(R.layout.initial)
+            setContentView(R.layout.activity_splash_one)
+            val btnOne: Button = findViewById(R.id.btn_one)
+            btnOne.setBackgroundColor(Color.parseColor("#7BAF8D"))
+            buttons()
+
         }, 3000
         )
+    }
+    fun buttons(){
+        val btnOne:Button = findViewById(R.id.btn_one)
+        val btnTwo:Button = findViewById(R.id.btn_two)
+        val btnTree:Button = findViewById(R.id.btn_tree)
+        val btnFour:Button = findViewById(R.id.btn_four)
+        val btnFive:Button = findViewById(R.id.btn_five)
+        val btnSix:Button = findViewById(R.id.btn_six)
+        val btnSeven:Button = findViewById(R.id.btn_seven)
+        val btnEight:Button = findViewById(R.id.btn_heigt)
+        btnOne.setOnClickListener {
+            btnOne.setBackgroundColor(Color.parseColor("#7BAF8D"))
+            btnTwo.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnTree.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnFour.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnFive.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnSix.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnSeven.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnEight.setBackgroundColor(Color.parseColor("#FFFFFF"))
+
+            setContentView(R.layout.activity_splash_one)
+        }
+        btnTwo.setOnClickListener {
+            btnOne.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnTwo.setBackgroundColor(Color.parseColor("#7BAF8D"))
+            btnTree.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnFour.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnFive.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnSix.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnSeven.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnEight.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            setContentView(R.layout.activity_splash_two)
+        }
+        btnTree.setOnClickListener {
+            btnOne.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnTwo.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnTree.setBackgroundColor(Color.parseColor("#7BAF8D"))
+            btnFour.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnFive.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnSix.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnSeven.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnEight.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            setContentView(R.layout.activity_splash_tree)
+        }
+        btnFour.setOnClickListener {
+            btnOne.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnTwo.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnTree.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnFour.setBackgroundColor(Color.parseColor("#7BAF8D"))
+            btnFive.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnSix.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnSeven.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnEight.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            setContentView(R.layout.activity_splash_four)
+        }
+        btnFive.setOnClickListener {
+            btnOne.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnTwo.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnTree.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnFour.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnFive.setBackgroundColor(Color.parseColor("#7BAF8D"))
+            btnSix.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnSeven.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            btnEight.setBackgroundColor(Color.parseColor("#FFFFFF"))
+            setContentView(R.layout.activity_splash_five)
+        }
+
     }
 }
